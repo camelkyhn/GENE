4e0bbb46b479cc6236e251f9e224f8df59c1288a
Kemal Kayahan <kemal.kayahan@broadangle.com>
2021-04-02 01:42:11 +0300

    Gene-1 Created extension methods.

---
+++ Gene.Middleware/Extensions/Claims.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Claims;
+
+namespace Gene.Middleware.Extensions
+{
+    public static class Claims
+    {
+        public static Guid? GetId(this ClaimsPrincipal principal)
+        {
+            return !string.IsNullOrEmpty(principal?.FindFirstValue(ClaimTypes.NameIdentifier)) ? Guid.Parse(principal.FindFirstValue(ClaimTypes.NameIdentifier)) : (Guid?)null;
+        }
+
+        public static string GetEmail(this ClaimsPrincipal principal)
+        {
+            return principal?.FindFirstValue(ClaimTypes.Email);
+        }
+    }
+}
---
+++ Gene.Middleware/Extensions/ServiceCollection.cs
@@ -0,0 +1,56 @@
+using System;
+using Gene.Middleware.Constants;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gene.Middleware.Extensions
+{
+    public static class ServiceCollection
+    {
+        public static void ConfigureServices<TContext>(this IServiceCollection services, string database) where TContext : DbContext
+        {
+            services.AddAuthenticationAndCookie();
+            services.ConfigureCookiePolicyOptions();
+            services.AddDbContext<TContext>(options => options.UseSqlServer(database));
+            services.AddHttpsRedirection(options =>
+            {
+                options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+                options.HttpsPort          = 5001;
+            });
+            services.AddControllersWithViews();
+            services.AddRazorPages(options =>
+            {
+                // Redirect "/" request to "/Identity/Account/Login" route.
+                options.Conventions.AddAreaPageRoute(Areas.Identity, $"/{Controllers.Account}/{Actions.Login}", "");
+            });
+        }
+
+        public static void ConfigureCookiePolicyOptions(this IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded    = _ => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+        }
+
+        public static void AddAuthenticationAndCookie(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie(options =>
+                    {
+                        // Cookie settings
+                        options.Cookie.HttpOnly   = false;
+                        options.ExpireTimeSpan    = TimeSpan.FromDays(1);
+                        options.LoginPath         = "/Identity/Account/Login";
+                        options.AccessDeniedPath  = "/Identity/Account/AccessDenied";
+                        options.LogoutPath        = "/Identity/Account/Logout";
+                        options.SlidingExpiration = true;
+                    });
+        }
+    }
+}
