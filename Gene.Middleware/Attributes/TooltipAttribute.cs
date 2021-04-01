eef0dfd1210f50dba96894ec10a4ec672d31e3bb
Kemal Kayahan <kemal.kayahan@broadangle.com>
2021-04-02 01:39:27 +0300

    Gene-1 Created model info attributes.

---
+++ Gene.Middleware/Attributes/TooltipAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Gene.Middleware.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class TooltipAttribute : Attribute
+    {
+        public string Message { get; set; }
+
+        public string Position { get; set; }
+
+        public TooltipAttribute() { }
+
+        public TooltipAttribute(string message, string position)
+        {
+            Message  = message;
+            Position = position;
+        }
+    }
+}
