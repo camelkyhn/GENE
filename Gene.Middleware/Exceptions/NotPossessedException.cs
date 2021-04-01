99cd511686c4842c16d166c7357425d93649b11d
Kemal Kayahan <kemal.kayahan@broadangle.com>
2021-04-02 01:39:50 +0300

    Gene-1 Created exception classes.

---
+++ Gene.Middleware/Exceptions/NotPossessedException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Gene.Middleware.Exceptions
+{
+    public class NotPossessedException : Exception
+    {
+        public NotPossessedException(string model) : base($"This {model} is not possessed by you!")
+        {
+        }
+    }
+}
---
+++ Gene.Middleware/Exceptions/OperationFailedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Gene.Middleware.Exceptions
+{
+    public class OperationFailedException : Exception
+    {
+        public OperationFailedException(string operation) : base($"An error occured while processing the {operation}!")
+        {
+        }
+
+        public OperationFailedException(string operation, string section) : base($"An error occured while processing the {operation} in the {section}!")
+        {
+        }
+    }
+}
